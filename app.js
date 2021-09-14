43e0d2bce121124d1fa5e44f443180aabac75abb
gonzalo <gonza.alt@hotmail.com.ar>
2021-09-14 17:07:42 -0300

    nuevo js repository

---
+++ app.js
@@ -0,0 +1,10 @@
+let nombre = prompt ("tu nombre aqui")
+let apellido = prompt("apellido")
+let lenguaje = prompt ("lenguaje")
+
+console.log(nombre)
+console.log(apellido)
+console.log(lenguaje)
+
+console.log(nombre + " " + apellido )
+console.log(`${nombre}  ${apellido}  ${lenguaje}`);
